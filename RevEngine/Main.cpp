fd623c65a6d40bbc35457110c7b0d800c2fcd66a
Joxx0r <Joxx0r@DESKTOP-581D0RH>
2021-03-15 18:28:47 +0100

    test commit to see git working

--- RevEngine/Main.cpp
+++ RevEngine/Main.cpp
@@ -16,7 +16,7 @@
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-    D3D12HelloTriangle sample(1280, 720, L"Rev Engine");
+    D3D12HelloTriangle sample(1280, 720, L"Revelations Engine");
    // D3D12HelloTriangleTwo sample(1280, 720, L"D3D12 Hello Triangle");
     return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
